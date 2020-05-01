06ed9d668035f35018c70692a24d044311ab0c5c
Ken Dacanay <ken.p.r.dacanay@gmail.com>
2020-05-01 16:21:27 -0400

    added quantity add functionality to manager.js

--- javascript/bamazonManager.js
+++ javascript/bamazonManager.js
@@ -42,7 +42,7 @@ function managerMenu() {
             } else if (answer.select === "VIEW LOW INVENTORY") {
                 lowInventory();
             } else if (answer.select === "ADD TO INVENTORY") {
-                addInventory();
+                addToInventory();
             } else if (answer.select === "ADD NEW PRODUCT") {
                 addProduct();
             } else {
@@ -53,9 +53,7 @@ function managerMenu() {
 
 function productView() {
 
-    console.log(chalk.bgBlue("\n=============================================================================="));
-    console.log(chalk.bgBlue("\n=========================Today's Available Products==========================="));
-    console.log(chalk.bgBlue("\n=============================================================================="));
+    console.log(chalk.bgBlue("\n=======================Available Products For Sale========================\n"));
 
     connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
@@ -87,27 +85,125 @@ function productView() {
 
 function lowInventory() {
     //connection query to select all products below <= 5
+    console.log(chalk.yellow("\n======================These Products Are Low on Stock============================\n"));
     connection.query("SELECT * FROM products WHERE STOCK_QUANTITY <= 5", function (err, res) {
         if (err) throw err;
         //display w/console.table
         console.table(res);
+        setTimeout(() => {
+
+            inquirer
+                .prompt({
+                    type: "list",
+                    name: "continue",
+                    message: "Return to Menu?",
+                    choices: [
+                        "YES",
+                        "NO"
+                    ]
+                })
+                .then(function (answer) {
+                    if (answer.continue === "YES") {
+                        managerMenu();
+                    } else if (answer.continue === "NO") {
+                        connection.end();
+                    }
+                })
+        }, 1000);
+    })
+}
+
+function addToInventory() {
+    //-------------display inventory
+    console.log(chalk.bgBlue("\n=======================Available Products For Sale========================\n"));
+
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        //load products w/console.table
+        console.table(res);
+    });
+    setTimeout(() => {
 
         inquirer
             .prompt({
-                type: "list",
-                name: "continue",
-                message: "Return to Menu?",
-                choices: [
-                    "YES",
-                    "NO"
-                ]
-            })
-            .then(function (answer) {
-                if (answer.continue === "YES") {
-                    managerMenu();
-                } else if (answer.continue === "NO") {
-                    connection.end();
+                type: "input",
+                name: "addToID",
+                message: "Please enter the Item ID: ",
+                validate: function (value) {
+                    if (value) {
+                        return true;
+                    }
+                    return false;
                 }
             })
-    })
+            .then(function (answer) {
+                connection.query("SELECT ITEM_ID, PRODUCT_NAME, DEPARTMENT_NAME, PRICE, STOCK_QUANTITY FROM products WHERE ?", {
+                    ITEM_ID: answer.addToID
+                }, function (err, res) {
+                    if (err) throw err;
+                    if (res) {
+                        displayItem = (chalk.cyan("Item Id: " + res[0].ITEM_ID + " | " + "Product: " + res[0].PRODUCT_NAME + " | " + " Price: " + res[0].PRICE));
+                        continueAdd(displayItem);
+                    }
+                })
+            })
+    }, 1000);
+}
+
+function continueAdd(displayItem) {
+    console.log(chalk.yellow("\nYou have selected: " + displayItem + "\n"));
+
+    inquirer
+        .prompt({
+            type: "list",
+            name: "confirm",
+            message: "Is This Correct?",
+            choices: [
+                "YES",
+                "NO"
+            ]
+        })
+        .then(function (answer) {
+            if (answer.confirm === "YES") {
+                finalizeAdd(displayItem);
+            } else if (answer.confirm === "NO") {
+
+                console.log(chalk.yellow("\n===================REDIRECTING======================="));
+                setTimeout(() => {
+                    addToInventory();
+                }, 1000);
+            }
+        })
+}
+
+function finalizeAdd(displayItem) {
+    var finalAddProduct = displayItem.split(" ", 3);
+
+    inquirer
+        .prompt({
+            type: "input",
+            name: "quantity",
+            message: "Enter Amount to Add to Product: " + displayItem,
+            validate: function (value) {
+                if (value) {
+                    return true;
+                }
+                return false;
+            }
+        })
+        .then((function (answer) {
+            connection.query("UPDATE products SET STOCK_QUANTITY = stock_quantity + ? WHERE ITEM_ID = ?",
+                [answer.quantity, finalAddProduct[2]],
+                function (err, res) {
+                    if (err) throw err;
+                    // console.log(res);
+                    if (res) {
+                        console.log(chalk.yellow("\n====================SUCCESS!======================="));
+                        console.log(chalk.yellow("\n====================REDIRECTING======================="));
+                        setTimeout(() => {
+                            managerMenu();
+                        }, 1000);
+                    }
+                })
+        }))
 }
